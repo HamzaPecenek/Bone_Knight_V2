2f7f25ce754e6fee459d5f1182b4ff813a3c9056
Alienated47 <andreas1942w@gmail.com>
2025-12-14 19:05:40 +0300

    Boss and Level6

---
+++ Assets/Scripts/boss/Follow.gd
@@ -0,0 +1,22 @@
+extends State
+
+func enter():
+	super.enter()
+	owner.set_physics_process(true)
+	animation_player.play("idle")
+	
+func exit():
+	super.exit()
+	owner.set_physics_process(false)
+	
+func transition():
+	var distance = owner.direction.length()
+	if distance < 45:
+		get_parent().change_state("MeeleAttack")
+	elif distance > 130:
+		var chance = randi() %2
+		match chance:
+			0:
+				get_parent().change_state("Missile")
+			1:
+				get_parent().change_state("LaserBeam")
---
+++ Assets/Scripts/healthbar_skelaton.gd
@@ -0,0 +1,17 @@
+extends ProgressBar
+class_name HealthBarEnemy
+
+@export var max_health: int = 100
+
+func _ready() -> void:
+	min_value = 0
+	max_value = max_health
+	value = max_health
+
+
+func set_health(current_health: int) -> void:
+	value = clamp(current_health, 0, max_health)
+
+
+func reset_health() -> void:
+	value = max_health
