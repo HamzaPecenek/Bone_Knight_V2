747d7f85981ae16a8047cb2e8ed580a4096da74c
HamzaPecenek <hamza.petschenek@gmail.com>
2025-12-15 17:13:02 +0300

    Added Ilsu's features starting from old version

--- Assets/Scripts/player.gd
+++ Assets/Scripts/player.gd
@@ -244,19 +244,3 @@ func update_respawn_point(new_position: Vector2) -> void:
 
 func _on_attack_area_p_body_exited(_body: Node2D) -> void:
 	pass
-
-
-func _on_interact_area_area_entered(area: Area2D) -> void:
-	pass # Replace with function body.
-
-
-func _on_interact_area_area_exited(area: Area2D) -> void:
-	pass # Replace with function body.
-
-
-func _on_interact_area_area_shape_entered(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
-	pass # Replace with function body.
-
-
-func _on_interact_area_area_shape_exited(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
-	pass # Replace with function body.
