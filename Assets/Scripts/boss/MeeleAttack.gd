1935a8fb7e5eae1dc98d44fb5a90e1141486a80a
Alienated47 <andreas1942w@gmail.com>
2025-12-23 02:21:52 +0300

    modified the boss to have melee damage, rebalanced the laser and missile damages, made doors locked untill all coins are collected - so coins in levels are tracked by the door to avoid any changes in inventory system. added doors and coins to other levels, however if you do not want that just skip those levels when copying them to your project

--- Assets/Scripts/boss/MeeleAttack.gd
+++ Assets/Scripts/boss/MeeleAttack.gd
@@ -1,9 +1,34 @@
 extends State
 
+@onready var meele = $"../../meele"
+var can_transition: bool = false
+
 func enter():
 	super.enter()
-	animation_player.play("meele_attack")
+	meele.monitoring = true
+	meele.collision_layer = 1
+	meele.collision_mask = 1
+	
+	_connect_signals()
+	
+	await play_animation("meele_attack")
 
-func transition(): 
-	if owner.direction.length() > 45:
+	meele.monitoring = false        # stop damaging after meele attack ends
+	can_transition = true
+
+func play_animation(anim_name):
+	animation_player.play(anim_name)
+	await animation_player.animation_finished
+	
+func _connect_signals():
+	if not meele.body_entered.is_connected(_on_meele_body_entered):
+		meele.body_entered.connect(_on_meele_body_entered)
+
+func _on_meele_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		body.take_damage(10)   # laser damage
+
+func transition():
+	if can_transition:
+		can_transition = false
 		get_parent().change_state("Follow")
